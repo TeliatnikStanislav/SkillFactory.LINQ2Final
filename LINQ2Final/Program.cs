ead3837ec0eb149c215b80dd113931618b179aca
Stanislav Teliatnik <136919802+TeliatnikStanislav@users.noreply.github.com>
2024-04-15 16:50:46 +0200

    SelectMany

--- LINQ2Final/Program.cs
+++ LINQ2Final/Program.cs
@@ -1,16 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace LINQ2Final
+namespace Task1
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
+            var classes = new[]
+            {
+               new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
+               new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
+               new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
+           };
+            var allStudents = GetAllStudents(classes);
 
+            Console.WriteLine(string.Join(" ", allStudents));
+            Console.ReadKey();
+        }
+
+        static string[] GetAllStudents(Classroom[] classes)
+        {
+            var allStudents = classes.SelectMany(classroom => classroom.Students).ToArray();
+            return allStudents;
+        }
+
+        public class Classroom
+        {
+            public List<string> Students = new List<string>();
         }
     }
 }
